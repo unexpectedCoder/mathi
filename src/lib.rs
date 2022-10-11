b2ddcdfcf42eb2a0f6ca6ec6441a9a1e8c84c303
Vadim Fedulov <avtobusikstoy@gmail.com>
2022-10-11 10:15:29 +0300

    Added isclose method

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,27 @@
 use core::slice;
 
-pub struct Vector<T> {
-    arr: Vec<T>
+#[derive(Debug)]
+pub struct Vector {
+    arr: Vec<f64>
 }
 
-impl<T> Vector<T>
-    where T: Copy + Clone
-{
-    pub fn new(arr: &[T]) -> Vector<T> {
+impl Vector {
+    pub fn new(arr: &[f64]) -> Vector {
         Vector { arr: arr.to_owned() }
     }
 
-    pub fn iter(&self) -> slice::Iter<T> {
+    pub fn iter(&self) -> slice::Iter<f64> {
         self.arr.iter()
+    }
+
+    pub fn isclose(v: &Vector, val: f64, tol: Option<f64>) -> bool {
+        let tol = tol.unwrap_or(1e-6);
+        for vi in v.iter() {
+            if (*vi - val).abs() > tol {
+                return false;
+            }
+        }
+        true
     }
 }
 
@@ -27,5 +36,16 @@ mod tests {
         for (vi, ai) in v.iter().zip(&arr) {
             assert_eq!(vi, ai);
         }
+    }
+
+    #[test]
+    fn test_isclose() {
+        let val = 19.1;
+        let v = Vector::new(&[1., 2., 3.]);
+        assert!(!Vector::isclose(&v, val, None));
+        let v = Vector::new(&[1., 2., 3. * (val / 3.)]);
+        assert!(!Vector::isclose(&v, val, Some(1e-3)));
+        let v = Vector::new(&[val, val, 3. * (val / 3.)]);
+        assert!(Vector::isclose(&v, val, None));
     }
 }
